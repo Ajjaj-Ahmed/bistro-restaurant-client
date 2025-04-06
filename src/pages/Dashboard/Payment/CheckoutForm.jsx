10226c98c2d997a6e96452f140aa966d13ab1f10
Ajjaj Ahmed <ajjajahmedasif@gmail.com>
2025-04-06 21:44:06 +0600

    set client secret

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,39 +1,69 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxiosSecure from "../../../hooks/useAxiosSecure"
+import useCart from "../../../hooks/useCart"
 
 const CheckoutForm = () => {
+    const [error, setError] = useState('');
     const stripe = useStripe();
-    const elements = useElements()
-    const handleSubmit = async (event) =>{
+    const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const [clientSecret, setClientSecret] = useState('');
+    const [cart]= useCart();
+    const totalPrice = cart.reduce((total, item)=>total+item.price, 0);
+
+    useEffect(()=>{
+        axiosSecure.post('/create-payment-intent',{price : totalPrice})
+        .then(res=>{
+            console.log(res.data.clientSecret);
+            setClientSecret(res.data.clientSecret);
+        })
+    }, [axiosSecure, totalPrice])
+
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return
         }
         const card = elements.getElement(CardElement)
-        if(card == null){
+        if (card == null) {
             return
+        }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+        if (error) {
+            console.log('payment error', error)
+            setError(error.message)
+        }
+        else {
+            console.log('payment method', paymentMethod);
+            setError('');
         }
     }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <CardElement
-            options={{
-                style:{
-                    base:{
-                        fontSize:'16px',
-                        color:'#424770',
-                        '::placeholder':{
-                            color:'#aab7c4'
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4'
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
                         },
                     },
-                invalid:{
-                        color:'#9e2146',
-                    },
-                },
-            }}
+                }}
             >
-            <button type='submit' disabled={!stripe}></button>
             </CardElement>
+            <button className='btn btn-sm btn-primary my-4' type='submit' disabled={!stripe}>Pay</button>
+                <p className="text-red-600">{error}</p>
         </form>
     );
 };
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+// VITE_Payment_Gateway_PK=pk_test_51RAmG3IzAIJkATc0OtP9evILKW3kK9A015UiOH9uAZJv55B03pqZu2EjsXsrIos6XXIdcDfIo7PgJJh0cBRF8y5800LNzUht3n
 
 // add publishible key
-const stripePromise = loadStripe('')
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
 const Payment = () => {
 
     return (
@@ -12,7 +15,7 @@ const Payment = () => {
             <SectionTitle heading={'Payment'} subHeading={'Please pay to eat'}></SectionTitle>
             <div>
                 <Elements stripe={stripePromise}>
-            
+                    <CheckoutForm></CheckoutForm>
                 </Elements>
             </div>
         </div>
